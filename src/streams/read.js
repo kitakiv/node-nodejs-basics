849570baeb0c6f1ca06c92f4ef682d6baee32806
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-10-04 23:04:32 +0300

    feat: create read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import process from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+    readStream.on('data', (chunk) => {
+        process.stdout.write(`${chunk.toString()} \n`);
+    });
+
 };
 
 await read();
