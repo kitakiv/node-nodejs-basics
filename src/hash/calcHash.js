29d99d9ed409bdf4723aede5c108d13dffd10342
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-10-04 22:38:03 +0300

    feat: done task hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+const { createHash } = await import('node:crypto');
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { create } from 'node:domain';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('SHA256');
+
+    createReadStream(filePath, { encoding: 'utf8' })
+        .on('data', (data) => {
+            hash.update(data);
+        })
+        .on('end', () => {
+            console.log(hash.digest('hex'));
+        });
 };
 
 await calculateHash();
