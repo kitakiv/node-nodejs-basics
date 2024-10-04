5554676ffb758d9be7622c5e8483f69251fa8f3a
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-10-04 21:29:38 +0300

    feat: end cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arrayArgs = process.argv.slice(2);
+    const result = arrayArgs.reduce((acc, arg) => {
+        if (arg.includes('--')) {
+            arg.replace('--', '');
+            acc.push([arg]);
+        } else {
+            acc.at(-1).push(arg);
+        }
+        return acc
+    }, []);
+    const strValues = result.map(argAndValue => argAndValue.join(' is ')).join(', ');
+
+    console.log(strValues);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import process from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+   const evnVars = process.env;
+   const rssKeys = Object.keys(evnVars).filter((key) => key.startsWith('RSS_'));
+   const result = rssKeys.reduce((acc, key) => {
+       const value = evnVars[key];
+       acc.push(`${key} = ${value}`);
+       return acc;
+   }, []);
+   console.log(result.join('; '));
 };
 
 parseEnv();
