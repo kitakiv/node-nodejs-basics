8bf1e81250b9e29b7ab4ff486239af136d95afab
Viktoriya Nykytenko <kitakiv@gmail.com>
2024-10-06 21:49:15 +0300

    feat: end esm modules task

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import './files/c.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', { with: { type: "json" } }).then(module => module.default);
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', { with: { type: "json" } }).then(module => module.default);
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +38,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
